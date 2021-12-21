0d09af27aca6eb9b9b6f35202f6e44e467f45d68
Egor Nagibin <e.s.nagibin@tmn3.etagi.com>
2021-12-21 15:36:15 +0500

    master

---
+++ App.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+
+// const Section = ({children, title}): Node => {
+// npx react-native run-ios --simulator="iPhone SE (2nd generation)"
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello world</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
