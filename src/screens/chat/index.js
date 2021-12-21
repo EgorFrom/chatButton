7c004897b763c60d4d243947f8bc8a2f870d7c59
Egor Nagibin <e.s.nagibin@tmn3.etagi.com>
2021-12-22 02:58:00 +0500

    main change delay logic

--- src/screens/chat/index.js
+++ src/screens/chat/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { StyleSheet, FlatList, Button, Dimensions, View } from 'react-native';
 import Row from './components/Row';
 import { getRandom } from './utils';
@@ -10,10 +10,26 @@ const minMessageWidth = 60;
 const Chat = () => {
   const scrollRef = useRef(null);
   const [list, setList] = useState([]);
+  const [events, setEvents] = useState([]);
   const [disabled, setDisabled] = useState(false);
 
-  const addMessage = timestamp => setList((list) => {
-    checkPushAvailable(list);
+  useEffect(() => {
+    const flatList = list.flatMap(item => item);
+    if (flatList?.length) {
+      const newEventList = events
+        .splice(events.length === 3 ? 1 : 0, 2)
+        .concat(flatList[flatList.length - 1].timestamp);
+      if (needDelay(newEventList)) {
+        setDisabled(true);
+        setTimeout(() => setDisabled(false), 2000)
+      }
+      setEvents(newEventList);
+    }
+  }, [list.length]);
+
+  const needDelay = list => list.length > 2 && Math.abs(list[0] - list[list.length - 1]) < 1000
+
+  const handleOnPress = ({ nativeEvent: { timestamp } }) => setList((list) => {
     const item = getMessage({ list, timestamp, width: getRandom(minMessageWidth, screenWidth) });
 
     if (!list.length) {
@@ -24,17 +40,6 @@ const Chat = () => {
       ? list.map((row, index) => index === list.length - 1 ? [...list[list.length - 1], item] : row)
       : list.concat([[item]]);
   });
-
-  const checkPushAvailable = (list) => {
-    const flatList = list.flatMap(item => item);
-    const delay = flatList.reduce((diff, message, index) => flatList.length - index < 3 ? message.timestamp - diff : diff, 0);
-    if (delay < 1000) {
-      setDisabled(true);
-      setTimeout(() => setDisabled(false), 5000)
-    }
-  }
-
-  const handleOnPress = ({ nativeEvent: { timestamp } }) => addMessage(timestamp);
 
   const scrollToEnd = () => scrollRef.current.scrollToEnd();
 
